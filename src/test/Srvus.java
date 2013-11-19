9802023494f956d0d174880fa6368e527d0d5fd2
Matheus Salmi <mathsalmi@gmail.com>
2013-11-19 15:08:42 -0200

    Renaming classes

--- src/test/Srvus.java
+++ src/test/Srvus.java
@@ -1,4 +1,4 @@
-package kindr;
+package test;
 
 import java.io.PrintWriter;
 import java.net.Socket;
--- src/test/Test.java
+++ src/test/Test.java
@@ -1,10 +1,10 @@
-package kindr;
+package test;
 
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Kindr {
+public class Test {
 
 	public static void main(String[] args) throws IOException {
 		ServerSocket ss = null;
