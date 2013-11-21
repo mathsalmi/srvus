830f882096c5d168f262774489c752999715ca2b
Matheus Salmi <mathsalmi@gmail.com>
2013-11-21 15:56:41 -0200

    Adding entities for message header fields

---
+++ src/entities/RequestField.java
@@ -0,0 +1,9 @@
+package entities;
+
+/**
+ * Represents a request header field
+ * @author Matheus Salmi
+ */
+public class RequestField extends HeaderField {
+
+}
---
+++ src/entities/ResponseField.java
@@ -0,0 +1,9 @@
+package entities;
+
+/**
+ * Represents a response header field
+ * @author Matheus Salmi
+ */
+public class ResponseField extends HeaderField {
+
+}
