f4bb5ab6e403883244f617c1e5221b13a51364dd
Matheus Salmi <mathsalmi@gmail.com>
2013-11-22 10:33:40 -0200

    Adding exception classes

---
+++ src/exception/HttpException.java
@@ -0,0 +1,23 @@
+package exception;
+
+import enums.EStatusCode;
+
+/**
+ * Thrown when there is a problem while processing HTTP message
+ * @author Matheus Salmi
+ */
+public class HttpException extends RuntimeException {
+	private static final long serialVersionUID = -3723812818550227259L;
+	private EStatusCode statusCode = null;
+	
+	public HttpException(EStatusCode statusCode) {
+		this.statusCode = statusCode;
+	}
+
+	/**
+	 * @return the statusCode
+	 */
+	public EStatusCode getStatusCode() {
+		return statusCode;
+	}
+}
