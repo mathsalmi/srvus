a3bc338e20fc685b6248cf5fde542c32ed18eab0
Matheus Salmi <mathsalmi@gmail.com>
2013-11-27 11:05:10 -0200

    Adding methods for discovering file extension

---
+++ src/util/FileUtil.java
@@ -0,0 +1,34 @@
+package util;
+
+import java.nio.file.Path;
+
+/**
+ * File util methods
+ * @author Matheus Salmi
+ */
+public class FileUtil {
+	private FileUtil() { }
+	
+	/**
+	 * Returns file extension given its name
+	 * @param name
+	 * @return
+	 */
+	public static String getExt(String name) {
+		if(name != null && ! name.isEmpty()) {
+			int index = name.lastIndexOf('.');
+			return name.substring(index + 1, name.length());
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * Returns file extension given its path
+	 * @param path
+	 * @return
+	 */
+	public static String getExt(Path path) {
+		return getExt(path.toString());
+	}
+}
