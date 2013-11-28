728a0759a402dfbc8257a06b7dfe40837cf6ef67
Matheus Salmi <mathsalmi@gmail.com>
2013-11-27 23:54:41 -0200

    Renaming DirUtil to PathUtil

--- src/util/PathUtil.java
+++ src/util/PathUtil.java
@@ -4,7 +4,8 @@ package util;
  * Contains the string paths for important directories
  * @author Matheus Salmi
  */
-public class DirUtil {
+public class PathUtil {
+	// TODO: read directories, paths and some behaviors from config files instead
 	public static final String ROOT_DIR = System.getProperty("user.dir");
 	public static final String SITES_DIR = ROOT_DIR + "/sites";
 	public static final String ETC_DIR = ROOT_DIR + "/etc";
